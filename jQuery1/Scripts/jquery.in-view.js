2f1fa8772cd28ebe7cbbdf92ac54ef0160b83908
Michael Koltashew <snake_kmm@mail.ru>
2014-07-15 01:43:16 +0300

    added in-view selector

---
+++ jQuery1/Scripts/jquery.in-view.js
@@ -0,0 +1,35 @@
+﻿(function ($) {
+
+    $.isHiddenAtTheBottom = function (element) {
+        var upper = $(window).height() + $(window).scrollTop();
+        return upper < $(element).offset().top;
+    };
+
+    $.isHiddenAtTheTop = function (element) {
+        var scrollTop = $(window).scrollTop();
+        return scrollTop > $(element).offset().top + $(element).height();
+    };
+
+    $.isHiddenOnTheRight = function (element) {
+        var fromLeft = $(window).width() + $(window).scrollLeft();
+        return fromLeft < $(element).offset().left;
+    };
+
+    $.isHiddenOnTheLeft = function (element) {
+        var scrollLeft = $(window).scrollLeft();
+        return scrollLeft > $(element).offset().left + $(element).width();
+    };
+
+    $.inView = function (element) {
+        var isInView = !$.isHiddenAtTheBottom(element) && !$.isHiddenAtTheTop(element)
+                       && !$.isHiddenOnTheRight(element) && !$.isHiddenOnTheLeft(element);
+        return isInView;
+    };
+
+    $.extend($.expr[':'], {
+        "in-view": function (a) {
+            return $.inView(a);
+        }
+    });
+
+})(jQuery);
